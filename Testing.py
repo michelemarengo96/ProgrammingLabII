f61262bd003c7d49c153fbf8fef6abf025139045
Michele Marengo <michele.marengo96@gmail.com>
2025-05-23 09:40:13 +0200

    1.0

---
+++ Testing.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a=np.array([1,2,3])
+print(a)
+
+b=np.arange(6)
+print(b)
+
+c=np.arange(1,6)
+print(c)
+
+d=np.arange(1,8,3)
+print(d)
+
+e=np.linspace(0,0.5,20)
+print(e)
+
+f=np.zeros_like(a,dtype="float")
+print(f)
