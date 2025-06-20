0c6a3b3844425f8c71bb5ffac229475986bab282
Michele Marengo <michele.marengo96@gmail.com>
2025-06-20 15:25:02 +0200

    3.2

--- Testing.py
+++ Testing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 affitti=np.array([720,980,650,1200], dtype="float64")
@@ -38,6 +39,29 @@ data = {
     'date_posted': ['2024-12-01', '2024-11-15', '2024-12-10']  # formato YYYY-MM-DD
 }
 
-df = pd.read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv")
+url="https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 
-print(df.sort_values(by="beer_servings",ascending=False))
+df=pd.read_csv(url)
+
+df_fare=df.sort_values(by="Fare", ascending=False).head(10)
+
+media_età=df['Age'].mean()
+media_fare=df.Fare.mean()
+media_sis=df.SibSp.mean()
+
+df['Family_index']=(df.SibSp+df.Parch)/df.Fare.where(df.Fare != 0, np.nan)
+
+passeggero_massimo=df['Family_index'].idxmax()
+
+
+
+più_100=df[df['Fare'] > 100].sort_values(by='Fare',ascending=False)
+
+plt.figure(figsize=(5,5))
+plt.bar(più_100['Name'].head(10), più_100['Fare'].head(10), color='red')
+
+df_last=df[df['Age'].notna() & df['Fare'].notna()]
+
+df_last.plot(y='Age',x='Fare',kind="line")
+
+
---
+++ Testing2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url="https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+
+df=pd.read_csv(url)
+
+
+embarked=df['Embarked'].mode()[0]
+
+
+df=df.dropna(subset=['Age'])
+
+df['Embarked']=df['Embarked'].fillna(embarked)
+
+test=df[df.duplicated(subset=['Age'])]
+
+
+media_classe=df.groupby('Pclass')['Age'].mean()
+
+print(media_classe)
+
